f4ccb6c50e5edc0dc474a7a0b46800f482d6f065
Adam Smith <volcom_stone664@hotmail.com>
2024-02-23 22:38:07 +1100

    added unique array of exercises from db

--- add-exercise.php
+++ add-exercise.php
@@ -2,3 +2,33 @@
 
 require_once __DIR__ . '/connection.php';
 
+$stmt = $conn->prepare("SELECT `exerciseName` FROM `exercises`");
+$stmt->execute();
+
+$listOfExercises = $stmt->fetchAll();
+
+
+// create list of unique exercises
+$uniqueExerciseArray = [];
+foreach ($listOfExercises as $exercise) {
+    if (!in_array($exercise, $uniqueExerciseArray)) {
+        $uniqueExerciseArray[] = $exercise;
+    }
+}
+
+var_dump($uniqueExerciseArray);
+
+
+$newExercise = [];
+
+
+
+?>
+
+<select>
+    <?php foreach ($uniqueExerciseArray as $exercise) : ?>
+        <option value='<?php echo $exercise?>'>
+            <?php echo $exercise['exerciseName'] ?>
+        </option>
+    <?php endforeach ?>
+</select>
