fc300420572a2fdedd43a818681d63710887ba46
Adam Smith <volcom_stone664@hotmail.com>
2024-02-23 16:53:08 +1100

    query added for specific exercise

--- get-exercise.php
+++ get-exercise.php
@@ -2,20 +2,33 @@
 
 require_once __DIR__ . '/connection.php';
 
-$exerciseName = "Bench Press";
+if(isset($_POST['select-exercise'])) {
+    $exerciseName = $_POST['select-exercise'];
 
-$stmt = $conn->prepare("SELECT `id`, `exerciseName`, `sets`, `reps`, `weight`, `rpe` FROM `exercises` WHERE `exerciseName` = :exerciseName");
-$stmt->bindParam(':exerciseName', $exerciseName);
-$stmt->execute();
-$exerciseList = $stmt->fetchAll();
+    $stmt = $conn->prepare("SELECT `id`, `exerciseName`, `sets`, `reps`, `weight`, `rpe` FROM `exercises` WHERE `exerciseName` = :exerciseName");
+    $stmt->bindParam(':exerciseName', $exerciseName);
+    $stmt->execute();
+    $exerciseList = $stmt->fetchAll();
 
-// Output the results
-foreach ($exerciseList as $exercise) {
-    echo "ID: " . $exercise['id'] . "<br>";
-    echo "Exercise Name: " . $exercise['exerciseName'] . "<br>";
-    echo "Sets: " . $exercise['sets'] . "<br>";
-    echo "Reps: " . $exercise['reps'] . "<br>";
-    echo "Weight: " . $exercise['weight'] . "<br>";
-    echo "RPE: " . $exercise['rpe'] . "<br>";
-    echo "<hr>";
+    // Output the results
+    foreach ($exerciseList as $exercise) {
+        echo "ID: " . $exercise['id'] . "<br>";
+        echo "Exercise Name: " . $exercise['exerciseName'] . "<br>";
+        echo "Sets: " . $exercise['sets'] . "<br>";
+        echo "Reps: " . $exercise['reps'] . "<br>";
+        echo "Weight: " . $exercise['weight'] . "<br>";
+        echo "RPE: " . $exercise['rpe'] . "<br>";
+        echo "<hr>";
+    }
 }
+?>
+<form method="post">
+
+<select name="select-exercise" id="select-exercise">
+    <option value=""></option>
+    <option value="Bench Press">Bench Press</option>
+    <option value="Squat">Squat</option>
+</select>
+<input type="submit" />
+
+</form>
