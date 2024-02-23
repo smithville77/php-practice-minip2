8972ac947dfb0af67c713d7303d6ae193fc85315
Adam Smith <volcom_stone664@hotmail.com>
2024-02-23 15:58:06 +1100

    get by exercise type page added

---
+++ get-exercise.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/connection.php';
+
+$exerciseName = "Bench Press";
+
+$stmt = $conn->prepare("SELECT `id`, `exerciseName`, `sets`, `reps`, `weight`, `rpe` FROM `exercises` WHERE `exerciseName` = :exerciseName");
+$stmt->bindParam(':exerciseName', $exerciseName);
+$stmt->execute();
+$exerciseList = $stmt->fetchAll();
+
+// Output the results
+foreach ($exerciseList as $exercise) {
+    echo "ID: " . $exercise['id'] . "<br>";
+    echo "Exercise Name: " . $exercise['exerciseName'] . "<br>";
+    echo "Sets: " . $exercise['sets'] . "<br>";
+    echo "Reps: " . $exercise['reps'] . "<br>";
+    echo "Weight: " . $exercise['weight'] . "<br>";
+    echo "RPE: " . $exercise['rpe'] . "<br>";
+    echo "<hr>";
+}
+?>
