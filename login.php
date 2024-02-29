651cec01de42aa002cbf226af7c310e21588e636
Adam Smith <volcom_stone664@hotmail.com>
2024-03-01 08:11:41 +1100

    cleaned up html

--- login.php
+++ login.php
@@ -1,36 +1,28 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 </head>
+
 <body>
     <h2>Login</h2>
     <form method="post" action="login.php">
-    <label for="email">Email:</label><br>
+        <label for="email">Email:</label><br>
         <input type="email" id="email" name="email"><br>
         <label for="password">Password:</label><br>
         <input type="password" id="password" name="password"><br>
         <input type="submit" value="Login">
     </form>
 </body>
+
 </html>
 
 
 <?php
 require_once __DIR__ . '/connection.php';
-// register a new user for testing
-
-// $userEmail = "user1@gmail.com";
-// $userPassword = "123";
-
-// // register the user
-
-// $result = $auth->login($userEmail, $userPassword);
-
-// var_dump($result);
 
 
 require_once __DIR__ . '/connection.php';
--- register.php
+++ register.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register to log exercises</title>
 </head>
+
 <body>
     <h2>Registration</h2>
     <form action="register.php" method="post">
-        
+
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br><br>
-        
+
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br><br>
-        
+
         <input type="submit" value="Register">
     </form>
 </body>
+
 </html>
 
 
@@ -35,7 +38,7 @@ require_once __DIR__ . '/connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
-    
+
     $email = $_POST['email'];
     $password = $_POST['password'];
 
